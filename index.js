975cf12219fdd077c6762085a5322f06daad0421
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2020-05-02 10:04:14 +0100

    Updates: added time constraint and extra message

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const log = console.log;
 const sendMsg = require('./sender');
 
 cron.schedule('0 */3 * * *', () => {
+  // cron.schedule('*/20 * * * *', () => {
   sendMsg();
   log('Message sent!');
 });
